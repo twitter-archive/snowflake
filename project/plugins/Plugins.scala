50f74a58810cc8444238cd0756fd226d83665690
Ryan King <ryan@theryanking.com>
2010-05-25 18:20:01 -0700

    update standard-project to fix cap

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val twitterNest = "com.twitter" at "http://www.lag.net/nest"
-  val defaultProject = "com.twitter" % "standard-project" % "0.5.6"
+  val defaultProject = "com.twitter" % "standard-project" % "0.5.8"
 }
