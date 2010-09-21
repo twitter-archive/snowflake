3d5dd7daf14ebb2fcbf6d5721224784ccb149378
Ryan King <ryan@theryanking.com>
2010-09-21 14:07:23 -0700

    update to standard project 0.7.5

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,6 +2,6 @@ import sbt._
 
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val twitterNest = "com.twitter" at "http://www.lag.net/nest"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.1"
+  val twitterMaven = "twitter.com" at "http://maven.twttr.com/"
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.5"
 }
