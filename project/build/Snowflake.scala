84be895c00185fc97a32ce0e840f9bad508d8276
Ryan King <ryan@theryanking.com>
2010-09-21 18:06:47 -0700

    remove extraneous dependencies

--- project/build/Snowflake.scala
+++ project/build/Snowflake.scala
@@ -10,26 +10,14 @@ class SnowflakeProject(info: ProjectInfo) extends StandardProject(info) {
 
   // library dependencies
   // note that JARs in libs/ are also pulled in, and so are not mentioned here
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val markdownj = "markdownj" % "markdownj" % "1.0.2b4-0.3.0"
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.5.8"
   val slf4jLog = "org.slf4j" % "slf4j-log4j12" % "1.5.8"
   val log4j = "apache-log4j" % "log4j" % "1.2.15"
-  val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
-  val commonsLang = "commons-lang" % "commons-lang" % "2.2"
-  val oro = "oro" % "oro" % "2.0.7"
   val configgy = "net.lag" % "configgy" % "1.4.7"
-  val mockito = "org.mockito" % "mockito-core" % "1.8.1"
-  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.5"
   val commonsPool = "commons-pool" % "commons-pool" % "1.5.4"
-  val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
   val ostrich = "com.twitter" % "ostrich" % "1.1.24"
   val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1"
-  val asm = "asm" % "asm-all" % "2.2"
-  val objenesis = "org.objenesis" % "objenesis" % "1.1"
-  val json = "com.twitter" % "json" % "1.1"
   val sp = "org.scala-tools.testing" % "specs"  % "1.6.2.2"
-  val javautils = "org.scala-tools" % "javautils" % "2.7.4-0.1"
   val thrift = "thrift" % "libthrift" % "0.2.0"
   val zookeeperClient = "com.twitter" % "zookeeper-client" % "1.5"
 
