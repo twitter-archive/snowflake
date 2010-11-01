fe56a265faf56c8c77be29e8460d3489d7122d75
Ryan King <ryan@theryanking.com>
2010-11-01 10:37:10 -0700

    reconnect on errors

--- src/main/scala/com/twitter/service/snowflake/Reporter.scala
+++ src/main/scala/com/twitter/service/snowflake/Reporter.scala
@@ -10,11 +10,15 @@ import org.apache.scribe.scribe.Client
 import org.apache.thrift.protocol.{TBinaryProtocol, TProtocolFactory}
 import org.apache.thrift.transport.{TFramedTransport, TSocket}
 import org.apache.thrift.{TBase, TException, TFieldIdEnum, TSerializer, TDeserializer}
+import net.lag.logging.Logger
+
 
 class Reporter {
+  private val log = Logger.get
 
   lazy val serializer = new TSerializer(new TBinaryProtocol.Factory())
   val entries = new ArrayList[LogEntry](1)
+  var scribeClient = buildScribeClient
 
   //cargo-culted from rockdove
   type TTBase = TBase[_ <: TFieldIdEnum]
@@ -24,13 +28,16 @@ class Reporter {
       entries.add(new LogEntry("snowflake", serialize(struct)))
       scribeClient.Log(entries)
     } catch {
-      case e: Exception => println(e)
+      case e: Exception => {
+        // logError(e)
+        scribeClient = buildScribeClient
+      }
     } finally {
       entries.clear
     }
   }
 
-  lazy val scribeClient = {
+  def buildScribeClient(): Client = {
     var sock = new TSocket(new Socket("localhost", 1463))
     var transport = new TFramedTransport(sock)
     var protocol = new TBinaryProtocol(transport, false, false)
@@ -40,5 +47,16 @@ class Reporter {
   def serialize[T <: TTBase](struct: T): String = {
     val b64 = new Base64(0)
     b64.encodeToString(serializer.serialize(struct)) + "\n"
+  }
+
+  def logError(e: Exception) {
+    // log.error(e, "Unexpected exception while scribing: %s", e.getMessage)
+    // log.error(e.getClass.getName + "\n" +
+    //   e.getStackTrace.map { st =>
+    //     import st._
+    //     "  "+getClassName+"."+getMethodName +
+    //     "("+getFileName+":"+getLineNumber+")"
+    //   }.mkString("\n")
+    // )
   }
 }
