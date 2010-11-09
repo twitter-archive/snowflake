f9588aafafb1043d52c3e2933cba95f8a2d9713c
Ryan King <ryan@theryanking.com>
2010-11-09 14:17:14 -0800

    some refactorings to Reporter.scala to make it more configurable

--- src/main/scala/com/twitter/service/snowflake/Reporter.scala
+++ src/main/scala/com/twitter/service/snowflake/Reporter.scala
@@ -1,61 +1,66 @@
 /** Copyright 2010 Twitter, Inc.*/
 package com.twitter.service.snowflake
 
+import com.twitter.ostrich.BackgroundProcess
+import com.twitter.ostrich.Stats
 import com.twitter.service.snowflake.gen.AuditLogEntry
+import java.net.ConnectException
 import java.net.Socket
 import java.util.ArrayList
+import java.util.concurrent.LinkedBlockingDeque
+import net.lag.configgy._
+import net.lag.logging.Logger
 import org.apache.commons.codec.binary.Base64;
 import org.apache.scribe.LogEntry
 import org.apache.scribe.scribe.Client
 import org.apache.thrift.protocol.{TBinaryProtocol, TProtocolFactory}
-import org.apache.thrift.transport.TTransportException
-import org.apache.thrift.transport.{TFramedTransport, TSocket}
+import org.apache.thrift.transport.{TTransportException, TFramedTransport, TSocket}
 import org.apache.thrift.{TBase, TException, TFieldIdEnum, TSerializer, TDeserializer}
-import net.lag.logging.Logger
-import java.util.concurrent.LinkedBlockingDeque
-import java.net.ConnectException
-import com.twitter.ostrich.BackgroundProcess
-import com.twitter.ostrich.Stats
 
 class Reporter {
   private val log = Logger.get(getClass.getName)
+  private val config = Configgy.config.configMap("snowflake.reporter")
+  private val scribe_category = config.getString("scribe_category", "snowflake_thrift")
+  private val scribe_host = config.getString("scribe_host", "localhost")
+  private val scribe_port = config.getInt("scribe_port", 1463)
+  private val scribe_socket_timeout = config.getInt("scribe_socket_timeout", 5000)
 
   type TTBase = TBase[_ <: TFieldIdEnum] //cargo-culted from rockdove
 
-  val queue = new LinkedBlockingDeque[TTBase](1000000) //TODO make this configurable
+  val queue = new LinkedBlockingDeque[TTBase](config.getInt("flush_queue_limit", 100000))
   private val structs = new ArrayList[TTBase](100)
   private val entries = new ArrayList[LogEntry](100)
   private var scribeClient: Option[Client] = None
   private val serializer = new TSerializer(new TBinaryProtocol.Factory())
 
-  Stats.makeGauge("scibe_flush_queue") { queue.size() }
+  Stats.makeGauge("scribe_flush_queue") { queue.size() }
   val enqueueFailuresCounter = Stats.getCounter("scribe_enqueue_failures")
 
   val thread = new BackgroundProcess("Reporter flusher") {
     def runLoop {
-      connect
-      queue.drainTo(structs, 100)
-      if (structs.size > 0) {
-        for (i <- 0 until structs.size) {
-          entries.add(i, new LogEntry("snowflake_thrift", serialize(structs.get(i)))) //TODO config
-        }
-        try {
-          scribeClient.get.Log(entries) //TODO timeout
+      try {
+        connect
+        queue.drainTo(structs, 100)
+        if (structs.size > 0) {
+          for (i <- 0 until structs.size) {
+            entries.add(i, new LogEntry(scribe_category, serialize(structs.get(i))))
+          }
+          scribeClient.get.Log(entries)
           log.trace("reported %d items to scribe. queue is %d".format(entries.size, queue.size))
-        } catch {
-          case e: TTransportException =>  { handle_exception(e, structs) }
-          case e: ConnectException    =>  { handle_exception(e, structs) }
-        } finally {
-          structs.clear
-          entries.clear
+        } else {
+          log.trace("no items. gonna back off")
+          Thread.sleep(1000)
         }
-      } else {
-        log.trace("no items. gonna back off")
-        Thread.sleep(1000)
+      } catch {
+        case e: TTransportException =>  { handle_exception(e, structs) }
+      } finally {
+        structs.clear
+        entries.clear
       }
     }
 
     private def handle_exception[T <: TTBase](e: Throwable, items: ArrayList[T]) {
+      //TODO put items back on queue
       scribeClient = None
 
       log.error("caught a thrift error. gonna chill for a bit. queue is %d".format(queue.size))
@@ -66,7 +71,8 @@ class Reporter {
     private def connect {
       while(scribeClient.isEmpty) {
         try {
-          var sock = new TSocket(new Socket("localhost", 1463)) //TODO make these configurable
+          var sock = new TSocket(scribe_host, scribe_port, scribe_socket_timeout)
+          sock.open()
           var transport = new TFramedTransport(sock)
           var protocol = new TBinaryProtocol(transport, false, false)
           scribeClient = Some(new Client(protocol, protocol))
