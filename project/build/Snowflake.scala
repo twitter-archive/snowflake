eaab986d36fb6154ca6517987bcd676478edb64a
Ryan King <ryan@theryanking.com>
2011-03-22 16:01:49 -0700

    update logging dependencies

--- project/build/Snowflake.scala
+++ project/build/Snowflake.scala
@@ -2,10 +2,9 @@ import sbt._
 import com.twitter.sbt._
 import Process._
 
-class SnowflakeProject(info: ProjectInfo) extends StandardServiceProject(info) with CompileThriftJava with CompileThriftRuby {
+class SnowflakeProject(info: ProjectInfo) extends StandardServiceProject(info) with CompileThriftJava with CompileThriftRuby with TartifactoryRepos {
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.5.8"
-  val slf4jLog = "org.slf4j" % "slf4j-log4j12" % "1.5.8"
-  val log4j = "apache-log4j" % "log4j" % "1.2.15"
+  val slf4jLog = "org.slf4j" % "slf4j-nop" % "1.5.8"
   val configgy = "net.lag" % "configgy" % "2.0.1"
   val commonsPool = "commons-pool" % "commons-pool" % "1.5.4"
   val ostrich = "com.twitter" % "ostrich" % "2.3.3"
@@ -13,6 +12,14 @@ class SnowflakeProject(info: ProjectInfo) extends StandardServiceProject(info) w
   val thrift = "thrift" % "libthrift" % "0.5.0"
   val commonsCodec = "commons-codec" % "commons-codec" % "1.4"
   val zookeeperClient = "com.twitter" % "zookeeper-client" % "2.0.0"
+
+  override def ivyXML =
+    <dependencies>
+      <exclude module="jms"/>
+      <exclude module="jmxtools"/>
+      <exclude module="jmxri"/>
+      <exclude org="apache-log4j"/>
+    </dependencies>
 
   override def pomExtra =
     <licenses>
