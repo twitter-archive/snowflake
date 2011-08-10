ed30cf30cdad39a54329a08458ecdbad2582af42
Arya Asemanfar <arya@twitter.com>
2011-08-10 09:06:19 -0700

    [split] upgrade snowflake to sbt-thrift-1.4.2

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -17,5 +17,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
    override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
   val defaultProject = "com.twitter" % "standard-project" % "0.12.7"
-  val sbtThrift = "com.twitter" % "sbt-thrift" % "1.1.0"
+  val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.4.2"
 }
