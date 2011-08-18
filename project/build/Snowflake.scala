865ea15497eeb69a5ace8360d618bbe030645883
Ryan King <ryan@twitter.com>
2011-08-18 14:40:46 -0700

    [split] Update to sbt-thrift 2.0.1
    This fixes snowflake in CI and makes it so that all thrift builds use a bundled binary rather than the system binary.

--- project/build/Snowflake.scala
+++ project/build/Snowflake.scala
@@ -2,18 +2,23 @@ import sbt._
 import com.twitter.sbt._
 import Process._
 
-class SnowflakeProject(info: ProjectInfo) extends StandardServiceProject(info) 
+class SnowflakeProject(info: ProjectInfo) extends StandardServiceProject(info)
   with DefaultRepos
   with CompileThriftJava
+  with CompileThriftRuby
+  with ProjectDependencies
   with NoisyDependencies {
   val slf4jApi = "org.slf4j" % "slf4j-api" % "1.5.8"
   val slf4jLog = "org.slf4j" % "slf4j-nop" % "1.5.8"
   val ostrich = "com.twitter" % "ostrich" % "4.8.2"
-  val sp = "org.scala-tools.testing" % "specs_2.8.0"  % "1.6.5"
+  val sp = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
   val thrift = "thrift" % "libthrift" % "0.5.0"
   val commonsCodec = "commons-codec" % "commons-codec" % "1.4"
   val zookeeperClient = "com.twitter" % "zookeeper-client" % "3.0.1"
-  val utilLogging = "com.twitter" % "util-logging" % "1.10.2"
+
+  projectDependencies(
+    "util" ~ "util-logging",
+    "util" ~ "util-thrift")
 
   override def ivyXML =
     <dependencies>
@@ -33,10 +38,10 @@ class SnowflakeProject(info: ProjectInfo) extends StandardServiceProject(info)
     </licenses>
 
   // generate a jar of just the thrift classes.
-    def clientPaths = (mainCompilePath ##) / "com" / "twitter" / "service" / "snowflake" / "gen" ** "*.class"
-    def packageClientAction = packageTask(clientPaths, outputPath, "snowflake-" + version.toString + "-thrift.jar", packageOptions).dependsOn(compile)
-    lazy val packageClient = packageClientAction
+  def clientPaths = (mainCompilePath ##) / "com" / "twitter" / "service" / "snowflake" / "gen" ** "*.class"
+  def packageClientAction = packageTask(clientPaths, outputPath, "snowflake-" + version.toString + "-thrift.jar", packageOptions).dependsOn(compile)
+  lazy val packageClient = packageClientAction
 
-    override def packageAction = super.packageAction.dependsOn(packageClientAction)
-    override def artifacts = Set(Artifact("snowflake", "thrift"))
+  override def packageAction = super.packageAction.dependsOn(packageClientAction)
+  override def artifacts = Set(Artifact("snowflake", "thrift"))
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,21 +1,20 @@
 import sbt._
 
-
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-   import scala.collection.jcl
-   val environment = jcl.Map(System.getenv())
-   def isSBTOpenTwitter = environment.get("SBT_OPEN_TWITTER").isDefined
-   def isSBTTwitter = environment.get("SBT_TWITTER").isDefined
+  import scala.collection.jcl
+  val environment = jcl.Map(System.getenv())
+  def isSBTOpenTwitter = environment.get("SBT_OPEN_TWITTER").isDefined
+  def isSBTTwitter = environment.get("SBT_TWITTER").isDefined
 
-   override def repositories = if (isSBTOpenTwitter) {
-     Set("twitter.artifactory" at "http://artifactory.local.twitter.com/open-source/")
-   } else if (isSBTTwitter) {
-     Set("twitter.artifactory" at "http://artifactory.local.twitter.com/repo/")
-   } else {
-     super.repositories ++ Seq("twitter.com" at "http://maven.twttr.com/")
-   }
-   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
+  override def repositories = if (isSBTOpenTwitter) {
+    Set("twitter.artifactory" at "http://artifactory.local.twitter.com/open-source/")
+  } else if (isSBTTwitter) {
+    Set("twitter.artifactory" at "http://artifactory.local.twitter.com/repo/")
+  } else {
+    super.repositories ++ Seq("twitter.com" at "http://maven.twttr.com/")
+  }
+  override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
   val defaultProject = "com.twitter" % "standard-project" % "0.12.10"
-  val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.4.2"
+  val sbtThrift = "com.twitter" % "sbt-thrift" % "2.0.1"
 }
